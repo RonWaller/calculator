fc3d31189d6f37c707b2da693fd5fdc070a2e864
RonWaller <ron.waller7@gmail.com>
2018-09-28 22:08:41 -0400

    Adding more functionality

--- js/main.js
+++ js/main.js
@@ -12,5 +12,29 @@ for (let i = 0; i < calcButton.length; i++) {
 
 function displayValue(value) {
     let display = document.querySelector('.display');
-    display.innerHTML += `${value}`;
+    if (value == '=') {
+        evaluate(display);
+    } else if (value == 'C') {
+        clear(display);
+    } else if (value == 'back' && display.innerHTML.length > 0) {
+        backspace(display);
+    } else {
+        display.innerHTML += `${value}`;
+    }
+}
+
+function evaluate(display) {
+    let answer = eval(display.innerHTML);
+    console.log(answer);
+    display.innerHTML = answer;
+}
+
+function clear(display) {
+    display.innerHTML = ' ';
+}
+
+function backspace(display) {
+    let string = display.innerHTML;
+    let newString = string.slice(0, -1);
+    display.innerHTML = newString;
 }
